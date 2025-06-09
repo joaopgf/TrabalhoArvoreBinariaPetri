dd6afd8c70a3f485457e5d4c67ddbb0bf9563aa5
João Pedro <joaopgf.it@gmail.com>
2025-06-09 14:43:49 -0300

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     ArvoreBinaria arvore = new ArvoreBinaria(50);
     arvore.inserir(30);
     arvore.inserir(70);
@@ -14,8 +16,9 @@ public class Main {
     arvore.inserir(26);
     arvore.inserir(24);
     arvore.inserir(23);
+//    arvore.inserir(sc.nextInt());
     arvore.visualizar();
-        System.out.println(" ");
+    System.out.println(" ");
     arvore.removerNo1(50);
     arvore.visualizar();
 
